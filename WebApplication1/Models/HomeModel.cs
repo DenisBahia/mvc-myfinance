9e5f15d1826b3da745fb0fd5dd9aecdd230d561d
DenisBahia <45142971+DenisBahia@users.noreply.github.com>
2019-03-07 07:32:01 -0300

    Initial commit

---
+++ WebApplication1/Models/HomeModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApplication1.Util;
+
+namespace WebApplication1.Models
+{
+    public class HomeModel
+    {
+        public string LerPlanoConta()
+        {
+            DAL objDAL = new DAL();
+            DataTable dt = objDAL.RetDataTable("select * from plano_contas");
+            return dt.Rows[0][0].ToString();
+        }
+    }
+}
---
+++ WebApplication1/Util/DAL.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Data;
+
+namespace WebApplication1.Util
+{
+    public class DAL
+    {
+
+        private static string server = "localhost";
+        private static string database = "financeiro";
+        private static string user = "root";
+        private static string password = "";
+
+        private string connectionString = $"Server={server};database={database};uid={user};pwd={password}";
+
+        private MySqlConnection connection;
+
+        public DAL()
+        {
+            connection = new MySqlConnection(connectionString);
+            connection.Open();
+        }
+
+        public DataTable RetDataTable(string sql)
+        {
+            DataTable dataTable = new DataTable();
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            MySqlDataAdapter adapter = new MySqlDataAdapter(command);
+            adapter.Fill(dataTable);
+            return dataTable;
+        }
+
+        public void ExecutarComandoSQL(string sql)
+        {
+            MySqlCommand command = new MySqlCommand(sql, connection);
+            command.ExecuteNonQuery();
+        }
+
+    }
+}
